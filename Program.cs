edc4da929e22c990d63518decc8766311d23d635
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-30 17:32:24 +0530

    [Harsh]add: created a new game class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Snake and Ladder game");
+            SnakeLadderGame game = new SnakeLadderGame();
+            game.Board();
+        }
+    }
+}
